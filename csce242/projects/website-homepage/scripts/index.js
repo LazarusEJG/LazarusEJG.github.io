f37ab4525b6cdd94d887d18d267b632d9298fa9e
LazarusEJG <ej2001e@gmail.com>
2026-09-20 23:43:21 -0400

    finished up homepage for project 04

--- csce242/projects/website-homepage/scripts/index.js
+++ csce242/projects/website-homepage/scripts/index.js
@@ -1,3 +1,4 @@
-document.getElementById("ani-value").innerHTML = 100+" ";
-document.getElementById("manga-value").innerHTML = 50+" ";
-document.getElementById("ln-value").innerHTML = 20+" ";
+aValue = document.getElementById("ani-value").innerHTML = 100;
+mValue = document.getElementById("manga-value").innerHTML = 50;
+lValue = document.getElementById("ln-value").innerHTML = 20;
+document.getElementById("stats-total").innerHTML = aValue+mValue+lValue+" total";
